e4d334bd84c905f8f47135cec21c54c5c210ff01
magz-27 <magz-27@users.noreply.github.com>
2025-01-20 18:18:04 +0100

    zaimportowano figury

--- main.py
+++ main.py
@@ -27,48 +27,62 @@ kogo_kolej = 0
 wybor = 1000
 poprawne_ruchy = []
 #import grafik
-black_queen = pygame.image.load('assets/images/black queen.png')
-black_queen = pygame.transform.scale(black_queen, (80, 80))
-black_queen_small = pygame.transform.scale(black_queen, (45, 45))
-black_king = pygame.image.load('assets/images/black king.png')
-black_king = pygame.transform.scale(black_king, (80, 80))
-black_king_small = pygame.transform.scale(black_king, (45, 45))
-black_rook = pygame.image.load('assets/images/black rook.png')
-black_rook = pygame.transform.scale(black_rook, (80, 80))
-black_rook_small = pygame.transform.scale(black_rook, (45, 45))
-black_bishop = pygame.image.load('assets/images/black bishop.png')
-black_bishop = pygame.transform.scale(black_bishop, (80, 80))
-black_bishop_small = pygame.transform.scale(black_bishop, (45, 45))
-black_knight = pygame.image.load('assets/images/black knight.png')
-black_knight = pygame.transform.scale(black_knight, (80, 80))
-black_knight_small = pygame.transform.scale(black_knight, (45, 45))
-black_pawn = pygame.image.load('assets/images/black pawn.png')
-black_pawn = pygame.transform.scale(black_pawn, (65, 65))
-black_pawn_small = pygame.transform.scale(black_pawn, (45, 45))
-white_queen = pygame.image.load('assets/images/white queen.png')
-white_queen = pygame.transform.scale(white_queen, (80, 80))
-white_queen_small = pygame.transform.scale(white_queen, (45, 45))
-white_king = pygame.image.load('assets/images/white king.png')
-white_king = pygame.transform.scale(white_king, (80, 80))
-white_king_small = pygame.transform.scale(white_king, (45, 45))
-white_rook = pygame.image.load('assets/images/white rook.png')
-white_rook = pygame.transform.scale(white_rook, (80, 80))
-white_rook_small = pygame.transform.scale(white_rook, (45, 45))
-white_bishop = pygame.image.load('assets/images/white bishop.png')
-white_bishop = pygame.transform.scale(white_bishop, (80, 80))
-white_bishop_small = pygame.transform.scale(white_bishop, (45, 45))
-white_knight = pygame.image.load('assets/images/white knight.png')
-white_knight = pygame.transform.scale(white_knight, (80, 80))
-white_knight_small = pygame.transform.scale(white_knight, (45, 45))
-white_pawn = pygame.image.load('assets/images/white pawn.png')
-white_pawn = pygame.transform.scale(white_pawn, (65, 65))
-white_pawn_small = pygame.transform.scale(white_pawn, (45, 45))
-white_images = [white_pawn, white_queen, white_king, white_knight, white_rook, white_bishop]
-small_white_images = [white_pawn_small, white_queen_small, white_king_small, white_knight_small,
-                      white_rook_small, white_bishop_small]
-black_images = [black_pawn, black_queen, black_king, black_knight, black_rook, black_bishop]
-small_black_images = [black_pawn_small, black_queen_small, black_king_small, black_knight_small,
-                      black_rook_small, black_bishop_small]
+czarny_hetman = pygame.image.load('assets/images/queen_dark.png')
+czarny_hetman = pygame.transform.scale(czarny_hetman, (80, 80))
+czarny_hetman_maly = pygame.transform.scale(czarny_hetman, (45, 45))
+
+czarny_krol = pygame.image.load('assets/images/king_dark.png')
+czarny_krol = pygame.transform.scale(czarny_krol, (80, 80))
+czarny_krol_maly = pygame.transform.scale(czarny_krol, (45, 45))
+
+czarna_wieza = pygame.image.load('assets/images/rook_dark.png')
+czarna_wieza = pygame.transform.scale(czarna_wieza, (80, 80))
+czarna_wieza_mala = pygame.transform.scale(czarna_wieza, (45, 45))
+
+czarny_goniec = pygame.image.load('assets/images/bishop_dark.png')
+czarny_goniec = pygame.transform.scale(czarny_goniec, (80, 80))
+czarny_goniec_maly = pygame.transform.scale(czarny_goniec, (45, 45))
+
+czarny_skoczek = pygame.image.load('assets/images/knight_dark.png')
+czarny_skoczek = pygame.transform.scale(czarny_skoczek, (80, 80))
+czarny_skoczek_maly = pygame.transform.scale(czarny_skoczek, (45, 45))
+
+czarny_pionek = pygame.image.load('assets/images/pawn_dark.png')
+czarny_pionek = pygame.transform.scale(czarny_pionek, (65, 65))
+czarny_pionek_maly = pygame.transform.scale(czarny_pionek, (45, 45))
+
+bialy_hetman = pygame.image.load('assets/images/queen_light.png')
+bialy_hetman = pygame.transform.scale(bialy_hetman, (80, 80))
+bialy_hetman_maly = pygame.transform.scale(bialy_hetman, (45, 45))
+
+bialy_krol = pygame.image.load('assets/images/king_light.png')
+bialy_krol = pygame.transform.scale(bialy_krol, (80, 80))
+bialy_krol_maly = pygame.transform.scale(bialy_krol, (45, 45))
+
+biala_wieza = pygame.image.load('assets/images/rook_light.png')
+biala_wieza = pygame.transform.scale(biala_wieza, (80, 80))
+biala_wieza_mala = pygame.transform.scale(biala_wieza, (45, 45))
+
+bialy_goniec = pygame.image.load('assets/images/bishop_light.png')
+bialy_goniec = pygame.transform.scale(bialy_goniec, (80, 80))
+bialy_goniec_maly = pygame.transform.scale(bialy_goniec, (45, 45))
+
+bialy_skoczek = pygame.image.load('assets/images/knight_light.png')
+bialy_skoczek = pygame.transform.scale(bialy_skoczek, (80, 80))
+bialy_skoczek_maly = pygame.transform.scale(bialy_skoczek, (45, 45))
+
+bialy_pionek = pygame.image.load('assets/images/pawn_light.png')
+bialy_pionek = pygame.transform.scale(bialy_pionek, (65, 65))
+bialy_pionek_maly = pygame.transform.scale(bialy_pionek, (45, 45))
+
+biale_obrazki = [bialy_pionek, bialy_hetman, bialy_krol, bialy_skoczek, biala_wieza, bialy_goniec]
+male_biale_obrazki = [bialy_pionek_maly, bialy_hetman_maly, bialy_krol_maly, bialy_skoczek_maly,
+                      biala_wieza_mala, bialy_goniec_maly]
+
+czarne_obrazki = [czarny_pionek, czarny_hetman, czarny_krol, czarny_skoczek, czarna_wieza, czarny_goniec]
+male_czarne_obrazki = [czarny_pionek_maly, czarny_hetman_maly, czarny_krol_maly, czarny_skoczek_maly,
+                       czarna_wieza_mala, czarny_goniec_maly]
+
 #główna pętla gry
 run = True;
 while run:
